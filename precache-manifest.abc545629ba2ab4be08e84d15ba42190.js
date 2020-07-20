388de68e43e3cd85a0b1721aea635f82d12c14bb
Samuel Kedziora <samukedo@gmail.com>
2020-07-20 23:35:43 +0200

    Updates

---
+++ precache-manifest.abc545629ba2ab4be08e84d15ba42190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673b8da228d936a368d248a55284faf2",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "4c99695a4e79451e384f",
+    "url": "/calculator/static/css/main.f18ac8f3.chunk.css"
+  },
+  {
+    "revision": "ecada51e1a3334c42b99",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.e6f076d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c99695a4e79451e384f",
+    "url": "/calculator/static/js/main.0e454ba8.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
